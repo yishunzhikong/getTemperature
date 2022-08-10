39ae0b33b67b37b934f7d8b3f902ba9a391cf1b0
yishunzhikong <yishunzhikong@163.com>
2022-08-10 22:49:09 +0800

    去掉缓存部分

--- getTemperature.py
+++ getTemperature.py
@@ -8,10 +8,10 @@ from openpyxl import load_workbook
 
 #必改数据
 #查询城市
-City = "beijing"
+City = "songpan"
 #查询开始&结束时间
-timeBegin = "20220630"
-timeEnd = "20220730"
+timeBegin = "20190815"
+timeEnd = "20190820"
 
 # URL常量
 website = "https://www.tianqi.com"
@@ -30,34 +30,23 @@ def getTemp( city, date):
     temp = re.search(Temperature, rs.text, re.S)
     return date,int(temp.group(1)),int(temp.group(2))
 
-#获取某个城市某段时间温度
-def getTemps(city, timebegin, timeend):
-
-    begin = datetime.date( \
-        int(timebegin[0:4]), \
-        int(timebegin[4:6]), \
-        int(timebegin[6:8]))
-    end = datetime.date( \
-        int(timeend[0:4]), \
-        int(timeend[4:6]), \
-        int(timeend[6:8]))
-    delta = datetime.timedelta(days=1)
-    temps = []
-
-    while begin <= end:
-        nowdate = begin.strftime("%Y%m%d")
-        temps.append(getTemp( City, nowdate))
-        begin += delta
-    
-    return temps
-
 # 主函数
 if __name__ == '__main__':
 
     i = 2
 
+    #限定时间范围
+    begin = datetime.date( \
+        int(timeBegin[0:4]), \
+        int(timeBegin[4:6]), \
+        int(timeBegin[6:8]))
+    end = datetime.date( \
+        int(timeEnd[0:4]), \
+        int(timeEnd[4:6]), \
+        int(timeEnd[6:8]))
+    delta = datetime.timedelta(days=1)
+
     #打开excel表格
-    temps = getTemps( City, timeBegin, timeEnd)
     tempWb = openpyxl.Workbook()
     tempWs = tempWb.active
     
@@ -66,12 +55,16 @@ if __name__ == '__main__':
     tempWs.cell(1, 2, value="min")
     tempWs.cell(1, 3, value="max")
 
-    #将数据写入excel表格
-    for tp in temps:
+    while begin <= end:
+        nowdate = begin.strftime("%Y%m%d")
+        tp = getTemp( City, nowdate)
         tempWs.cell(i, 1, value=tp[0])
         tempWs.cell(i, 2, value=tp[1])
         tempWs.cell(i, 3, value=tp[2])
+        begin += delta
         i += 1
+        #debug
+        print(tp)
 
     #保存表格文件
     tempWb.save(City + "_" + timeBegin + "_" +timeEnd + ".xlsx")
